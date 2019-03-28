c20cfd530792bbe04b0c8581bfe2f44b096ef138
ARENGASA <ARENGASA@192.168.1.100>
2019-03-28 09:17:45 +0100

    Input from user

--- FirstProgram/src/FirstProgram.java
+++ FirstProgram/src/FirstProgram.java
@@ -1,9 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
 public class FirstProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int i = 10;
-		int j = 20;
+		//int i = 10;
+		//int j = 20;
+		
+		BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("BufferedReader input String");
+		String test = reader.readLine();
+		System.out.println("Input String "+test);
+		
+		System.out.println("Enter i");
+		int i = sobj.nextInt();
+		
+		System.out.println("Enter j");
+		int j = sobj.nextInt();
+				
+		
 		
 		Calculations add = new Calculations(i, j);		
 		System.out.println("Hello World");
